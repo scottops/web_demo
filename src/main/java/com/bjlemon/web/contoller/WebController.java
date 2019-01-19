c61371fabbf879c99d5f7b682f9e5e20e7feeb2a
lianghui <1170464456@qq.com>
2019-01-19 15:55:07 +0800

    test

---
+++ src/main/java/com/bjlemon/web/contoller/WebController.java
@@ -0,0 +1,18 @@
+package com.bjlemon.web.contoller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/api/test")
+public class WebController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("v1:helloworld");
+    }
+
+}
